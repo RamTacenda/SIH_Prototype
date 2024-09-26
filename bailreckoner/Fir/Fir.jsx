16678936ed5a57a25b2bc6884dc5d781d9c77999
RamTacenda <kenanaljedi@gmail.com>
2024-09-26 11:23:15 +0530

    FIR has been added

---
+++ bailreckoner/Fir/Fir.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./style.css";
+
+export const Fir = () => {
+  return (
+    <div className="FIR">
+      <div className="div">
+        <div className="text-wrapper">FIR SEARCH</div>
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <div className="rectangle-2" />
+          <div className="text-wrapper-2">FIR No Search</div>
+        </div>
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="rectangle" />
+          <div className="text-wrapper-2">SEARCH</div>
+        </div>
+        <div className="overlap-2">
+          <div className="rectangle" />
+          <div className="rectangle" />
+          <div className="text-wrapper-3">FIR Period Search</div>
+        </div>
+        <div className="overlap-3">
+          <div className="rectangle" />
+          <div className="rectangle" />
+          <div className="text-wrapper-3">CLEAR</div>
+        </div>
+        <div className="div-wrapper">
+          <div className="text-wrapper-4">Seeking Help?</div>
+        </div>
+        <div className="text-wrapper-5">CITIZEN: Username</div>
+        <div className="navbar">
+          <div className="text-wrapper-6">DISTRICT:</div>
+          <div className="text-wrapper-6">DISTRICT:</div>
+          <div className="text-wrapper-6">DISTRICT:</div>
+          <div className="text-wrapper-6">DISTRICT:</div>
+        </div>
+        <div className="text-wrapper-7">POLICE STATION:</div>
+        <div className="overlap-4">
+          <div className="rectangle-3" />
+          <div className="rectangle-3" />
+          <div className="rectangle-3" />
+        </div>
+        <div className="rectangle-4" />
+        <div className="text-wrapper-8">FIR No:</div>
+        <div className="overlap-5">
+          <div className="rectangle-3" />
+          <div className="rectangle-3" />
+          <div className="rectangle-3" />
+        </div>
+      </div>
+    </div>
+  );
+};
